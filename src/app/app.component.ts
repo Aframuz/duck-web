27a604d4ce76b71095673b0aec441c89ddb2cfe4
aframuz <aframuz@tuta.io>
2024-11-14 10:47:05 -0300

    feat: clean component style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { DuckViewerComponent } from './features/duck-viewer/duck-viewer.component';
+import { DuckViewerComponent } from './components/duck-viewer/duck-viewer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, HttpClientModule, DuckViewerComponent],
   template: `
-    <app-duck-viewer></app-duck-viewer>
-  `
+    <div class="min-h-screen bg-gray-100 flex flex-col">
+      <h1 class="text-3xl font-bold text-center py-8 bg-white shadow-sm">Duck Image Viewer</h1>
+      <app-duck-viewer></app-duck-viewer>
+    </div>
+  `,
 })
 export class AppComponent { }
